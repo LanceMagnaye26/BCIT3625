e71b43345fc38ee51a6663e9da91e51645eb7b2a
LanceM26 <lancemagnaye@gmail.com>
2019-01-24 15:05:07 -0800

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <!-- header -->
+    <nav class="navbar">
+
+      <!-- logo -->
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          <img src="https://bulma.io/images/bulma-logo.png">
+        </a>
+      </div>
+    </nav>
+
+    <!-- routes will be rendered here -->
+    <router-outlet></router-outlet>
+
+    <!-- footer -->
+    <footer class="footer">
+    <div class="container">
+    <div class="content has-text-centered">
+      <p>
+        Made with <3 by <a href="https://twitter.com/chrisoncode">Chris from Scotch</a>
+      </p>
+    </div>
+    </div>
+    </footer>
+  `,
+  styles: []
 })
-export class AppComponent {
-  title = 'test-app';
-}
+export class AppComponent {}
